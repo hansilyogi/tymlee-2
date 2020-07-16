d9813517d821091b801277506bf5f5941eab3a41
ItFuturzTech9Teen <jagannathtech9teen@gmail.com>
2020-07-15 18:04:40 -0700

    booking slot api

--- public/scripts/controllers/bookingslotmaster.js
+++ public/scripts/controllers/bookingslotmaster.js
@@ -36,7 +36,7 @@ app.controller('BookingSlotMasterController', function($scope, $http) {
             companyId: $scope.CompanyId
         }
         $http({
-            url: imageroute + "/admin/getCompanyInventory",
+            url: imageroute + "/admin/getInventoryAndServiceListByCompanyId",
             method: "POST",
             cache: false,
             data: list,
@@ -45,10 +45,11 @@ app.controller('BookingSlotMasterController', function($scope, $http) {
             function(response) {
                 if (response.data.Data.length >= 1) {
                     $scope.InventoryList = response.data.Data;
-                    if ($scope.InventoryList.multipleServiceProviderRequired == true) {
-                        $scope.Multiple = true;
+                    if ($scope.InventoryList["0"].Inventory.multipleServiceProviderRequired == true) {
+                        $scope.serviceProviderDataList = true;
+                        console.log($scope.InventoryList["0"].serviceProviders["0"].serviceProviderName)
                     } else {
-                        $scope.Multiple = false;
+                        $scope.serviceProviderDataList = false;
                     }
 
                 } else {
@@ -62,56 +63,99 @@ app.controller('BookingSlotMasterController', function($scope, $http) {
     }
     $scope.GetInventory();
 
-    // $scope.GetBusinessCategoryType = function() {
-    //     $http({
-    //         url: imageroute + "/admin/CategoryMaster",
-    //         method: "POST",
-    //         cache: false,
-    //         data: {},
-    //         headers: { "Content-Type": "application/json; charset=UTF-8" },
-    //     }).then(
-    //         function(response) {
-    //             if (response.data.Data.length >= 1) {
-    //                 $scope.DataList = response.data.Data;
+    $scope.submitbookingslot = function() {
+        var json = {
+            "id": $scope.Id,
+            "companyId": $scope.CompanyId,
+            "inventoryId": $scope.InventoryId,
+            "serviceProviderId": $scope.ServiceProviderId,
+            "dayName": $scope.DayName,
+            "slotName": $scope.SlotName,
+            "fromTime": $scope.FromTime,
+            "toTime": $scope.ToTime,
+            "appointmentCount": $scope.AppointmentCount,
+            "rate": $scope.Rate
+        };
+        $http({
+            url: imageroute + "/admin/addSlot",
+            method: "POST",
+            data: json,
+            cache: false,
+            headers: { "Content-Type": "application/json; charset=UTF-8" },
+        }).then(function(response) {
+                if (response.data.Data == 1) {
+                    alert("Booking Slot Saved!");
+                    $("#modal-lg").modal("toggle");
+                    $scope.GetBookingSlot();
 
-    //             } else {
-    //                 $scope.DataList = [];
-    //             }
-    //         },
-    //         function(error) {
-    //             console.log("Insternal Server");
-    //         }
-    //     );
-    // }
-    // $scope.GetBusinessCategoryType();
+                } else {
+                    $scope.btnsave = false;
+                    alert("Unable to Save Booking Slot");
+                }
+            },
+            function(error) {
+                console.log(error);
+                $scope.btnsave = false;
+            }
+        );
+
+    }
+
+    $scope.GetBookingSlot = function() {
+        var list = {
+            companyId: $scope.CompanyId,
+            inventoryId: $scope.InventoryId,
+            serviceProviderId: $scope.ServiceProviderId
+        }
+        $http({
+            url: imageroute + "/admin/getSlot",
+            method: "POST",
+            cache: false,
+            data: list,
+            headers: { "Content-Type": "application/json; charset=UTF-8" },
+        }).then(
+            function(response) {
+                if (response.data.Data.length >= 1) {
+                    $scope.DataList = response.data.Data;
+
+                } else {
+                    $scope.DataList = [];
+                }
+            },
+            function(error) {
+                console.log("Insternal Server");
+            }
+        );
+    }
+    $scope.GetBookingSlot();
 
 
-    // $scope.DeleteData = function(id) {
-    //     var result = confirm("Are you sure you want to delete this ?");
-    //     if (result) {
-    //         $http({
-    //             url: imageroute + "/admin/deleteCategoryMaster",
-    //             method: "POST",
-    //             cache: false,
-    //             data: { id: id },
-    //             headers: { "Content-Type": "application/json; charset=UTF-8" },
-    //         }).then(
-    //             function(response) {
-    //                 if (response.data.Data == 1) {
-    //                     alert("Delete Successfully !");
-    //                     $scope.GetBusinessCategoryType();
+    $scope.DeleteData = function(id) {
+        var result = confirm("Are you sure you want to delete this ?");
+        if (result) {
+            $http({
+                url: imageroute + "/admin/deleteSlot",
+                method: "POST",
+                cache: false,
+                data: { id: id },
+                headers: { "Content-Type": "application/json; charset=UTF-8" },
+            }).then(
+                function(response) {
+                    if (response.data.Data == 1) {
+                        alert("Delete Successfully !");
+                        $scope.GetBookingSlot();
 
-    //                 } else {
-    //                     alert("Data Not deleted !");
-    //                     $scope.GetBusinessCategoryType();
-    //                 }
-    //             },
-    //             function(error) {
-    //                 console.log("Insternal Server");
-    //             }
-    //         );
-    //     }
-    // }
+                    } else {
+                        alert("Data Not deleted !");
+                        $scope.GetBookingSlot();
+                    }
+                },
+                function(error) {
+                    console.log("Insternal Server");
+                }
+            );
+        }
+    }
 
     // $scope.EditData = function(data) {
     //     $('#modal-lg').modal();
@@ -128,17 +172,16 @@ app.controller('BookingSlotMasterController', function($scope, $http) {
     // }
 
 
-    // $scope.Clear = function() {
-    //     $scope.Id = 0;
-    //     $scope.BusinessCategory = "";
-    //     $scope.StartDate = "";
-    //     $scope.BookingAmount = "";
-    //     $scope.ClientAmount = "";
-    //     $scope.RefundAmount = "";
-    //     $scope.CGSTPercent = "";
-    //     $scope.SGSTPercent = "";
-    //     $scope.IGSTPercent = "";
-    //     angular.element("input[type='file']").val(null);
-    // }
-    // $scope.Clear();
+    $scope.Clear = function() {
+        $scope.Id = 0;
+        $scope.DayName = "";
+        $scope.CompanyId = "";
+        $scope.InventoryId = "";
+        $scope.ServiceProviderId = "";
+        $scope.FromTime = "";
+        $scope.ToTime = "";
+        $scope.AppointmentCount = "";
+        $scope.Rate = "";
+    }
+    $scope.Clear();
 });
