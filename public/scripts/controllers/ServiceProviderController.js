3c2542cf2f7244f5ba90a0ba3d1570d072c304d0
YuG <macos@macOSs-iMac.local>
2020-09-21 23:35:37 +0530

    change class of service provider html controller £2

--- public/scripts/controllers/ServiceProviderController.js
+++ public/scripts/controllers/ServiceProviderController.js
@@ -11,152 +11,152 @@ app.controller('ServiceProviderController', function($scope, $http) {
         if (sessionStorage.getItem('Role') == 'company') {
             $scope.CompanyId = sessionStorage.getItem('SessionId')
         }
-
-        $scope.loadServiceProvider();
-        $scope.loadCompany();   
+        console.log('hello')
+        // $scope.loadServiceProvider();
+        // $scope.loadCompany();   
     }
-    $scope.loadServiceProvider = function() {
-        $scope.loading = true;
-        $http({
-            url: imageroute + "/admin/getServiceProvider",
-            method: "POST",
-            cache: false,
-            data: {companyId: $scope.CompanyId},
-            headers: { "Content-Type": "application/json; charset=UTF-8" },
-        }).then(
-            function(response) {
-                $scope.loading = false;
-                if (response.data.Data.length) {
-                    $scope.serviceProviders = response.data.Data;
-                } else {
-                    $scope.serviceProviders = [];
-                }
-            },
-            function(error) {
-                $scope.loading = false;
-                console.log("Insternal Server");
-            }
-        );
-    }
-    $scope.loadCompany = function() {
-            $scope.loading = true;
-            $http({
-                url: imageroute + "/admin/getCompanyMaster",
-                method: "POST",
-                cache: false,
-                data: {_id: $scope.CompanyId},
-                headers: { "Content-Type": "application/json; charset=UTF-8" },
-            }).then(
-                function(response) {
-                    $scope.loading = false;
-                    if (response.data.Data.length >= 1) {
-                        $scope.companyList = response.data.Data;
-                    } else {
-                        $scope.CompanyList = [];
-                    }
-                },
-                function(error) {
-                    $scope.loading = false;
-                    console.log("Insternal Server");
-                }
-            );
+    // $scope.loadServiceProvider = function() {
+    //     $scope.loading = true;
+    //     $http({
+    //         url: imageroute + "/admin/getServiceProvider",
+    //         method: "POST",
+    //         cache: false,
+    //         data: {companyId: $scope.CompanyId},
+    //         headers: { "Content-Type": "application/json; charset=UTF-8" },
+    //     }).then(
+    //         function(response) {
+    //             $scope.loading = false;
+    //             if (response.data.Data.length) {
+    //                 $scope.serviceProviders = response.data.Data;
+    //             } else {
+    //                 $scope.serviceProviders = [];
+    //             }
+    //         },
+    //         function(error) {
+    //             $scope.loading = false;
+    //             console.log("Insternal Server");
+    //         }
+    //     );
+    // }
+    // $scope.loadCompany = function() {
+    //         $scope.loading = true;
+    //         $http({
+    //             url: imageroute + "/admin/getCompanyMaster",
+    //             method: "POST",
+    //             cache: false,
+    //             data: {_id: $scope.CompanyId},
+    //             headers: { "Content-Type": "application/json; charset=UTF-8" },
+    //         }).then(
+    //             function(response) {
+    //                 $scope.loading = false;
+    //                 if (response.data.Data.length >= 1) {
+    //                     $scope.companyList = response.data.Data;
+    //                 } else {
+    //                     $scope.CompanyList = [];
+    //                 }
+    //             },
+    //             function(error) {
+    //                 $scope.loading = false;
+    //                 console.log("Insternal Server");
+    //             }
+    //         );
         
-    }
-    $scope.loadInventory = function(companyId) {
-        if (companyId) {
-            $scope.loading = true;
-            $http({
-                url: imageroute + "/admin/getInventoryByCompanyId",
-                method: "POST",
-                cache: false,
-                data: {companyId},
-                headers: { "Content-Type": "application/json; charset=UTF-8" },
-            }).then(
-                function(response) {
-                    $scope.loading = false;
-                    if (response.data.Data.length >= 1) {
-                        $scope.inventories = response.data.Data;
-                    } else {
-                        $scope.inventories = [];
-                    }
-                },
-                function(error) {
-                    $scope.loading = false;
-                    console.log("Insternal Server");
-                }
-            );
-        }
+    // }
+    // $scope.loadInventory = function(companyId) {
+    //     if (companyId) {
+    //         $scope.loading = true;
+    //         $http({
+    //             url: imageroute + "/admin/getInventoryByCompanyId",
+    //             method: "POST",
+    //             cache: false,
+    //             data: {companyId},
+    //             headers: { "Content-Type": "application/json; charset=UTF-8" },
+    //         }).then(
+    //             function(response) {
+    //                 $scope.loading = false;
+    //                 if (response.data.Data.length >= 1) {
+    //                     $scope.inventories = response.data.Data;
+    //                 } else {
+    //                     $scope.inventories = [];
+    //                 }
+    //             },
+    //             function(error) {
+    //                 $scope.loading = false;
+    //                 console.log("Insternal Server");
+    //             }
+    //         );
+    //     }
 
-    }
+    // }
 
-    $scope.onInverntorySelect = function(inventory) {
-        // $scope.model.serviceProvidername = inventory
-        $scope.model.appointmentMinutes = inventory.appointmentMinutes
-        $scope.model.rateType = inventory.rateType;
-        $scope.model.rateAmt = inventory.rateAmt;
-    }
+    // $scope.onInverntorySelect = function(inventory) {
+    //     // $scope.model.serviceProvidername = inventory
+    //     $scope.model.appointmentMinutes = inventory.appointmentMinutes
+    //     $scope.model.rateType = inventory.rateType;
+    //     $scope.model.rateAmt = inventory.rateAmt;
+    // }
 
-    $scope.onSubmit = function(){
-        $scope.loading = true;
-        $http({
-            url: imageroute + "/admin/addServiceProvider",
-            method: "POST",
-            cache: false,
-            data: this.model,
-            headers: { "Content-Type": "application/json; charset=UTF-8" },
-        }).then(function(response) {
-            if (response.data.IsSuccess == 1) {
-                alert("Service Provider Created!");
-                $("#modal-lg").modal("toggle");
-                $scope.loadServiceProvider();
+    // $scope.onSubmit = function(){
+    //     $scope.loading = true;
+    //     $http({
+    //         url: imageroute + "/admin/addServiceProvider",
+    //         method: "POST",
+    //         cache: false,
+    //         data: this.model,
+    //         headers: { "Content-Type": "application/json; charset=UTF-8" },
+    //     }).then(function(response) {
+    //         if (response.data.IsSuccess == 1) {
+    //             alert("Service Provider Created!");
+    //             $("#modal-lg").modal("toggle");
+    //             $scope.loadServiceProvider();
 
-            } else {
-                $scope.loading = false;
-                alert("Unable to Create Service provider");
-            }
-        },
-        function(error) {
-            console.log(error);
-            $scope.loading = false;
-        }
-    );
-    }
+    //         } else {
+    //             $scope.loading = false;
+    //             alert("Unable to Create Service provider");
+    //         }
+    //     },
+    //     function(error) {
+    //         console.log(error);
+    //         $scope.loading = false;
+    //     }
+    // );
+    // }
 
-    $scope.EditData = function(serviceProvider) {   
-        $scope.loadCompany();
-        $scope.loadInventory(serviceProvider.companyId._id)
-        let providerObj = Object.assign(JSON.parse(JSON.stringify(serviceProvider)), {companyId: serviceProvider.companyId._id, inventoryId: serviceProvider.inventoryId._id})
-        $scope.model = JSON.parse(JSON.stringify(providerObj));
-        setTimeout(function () {
-            $("#modal-lg").modal("toggle");
-        }, 350)
-    }
+    // $scope.EditData = function(serviceProvider) {   
+    //     $scope.loadCompany();
+    //     $scope.loadInventory(serviceProvider.companyId._id)
+    //     let providerObj = Object.assign(JSON.parse(JSON.stringify(serviceProvider)), {companyId: serviceProvider.companyId._id, inventoryId: serviceProvider.inventoryId._id})
+    //     $scope.model = JSON.parse(JSON.stringify(providerObj));
+    //     setTimeout(function () {
+    //         $("#modal-lg").modal("toggle");
+    //     }, 350)
+    // }
 
-    $scope.DeleteData = function(id) {
-        var result = confirm("Are you sure you want to delete this ?");
-        if (result) {
-            $http({
-                url: imageroute + "/admin/deleteServiceProvider",
-                method: "POST",
-                cache: false,
-                data: { id: id },
-                headers: { "Content-Type": "application/json; charset=UTF-8" },
-            }).then(
-                function(response) {
-                    if (response.data.Data == 1) {
-                        alert("Delete Successfully !");
-                        $scope.GetVendor();
+    // $scope.DeleteData = function(id) {
+    //     var result = confirm("Are you sure you want to delete this ?");
+    //     if (result) {
+    //         $http({
+    //             url: imageroute + "/admin/deleteServiceProvider",
+    //             method: "POST",
+    //             cache: false,
+    //             data: { id: id },
+    //             headers: { "Content-Type": "application/json; charset=UTF-8" },
+    //         }).then(
+    //             function(response) {
+    //                 if (response.data.Data == 1) {
+    //                     alert("Delete Successfully !");
+    //                     $scope.GetVendor();
 
-                    } else {
-                        alert("Data Not deleted !");
-                        $scope.GetVendor();
-                    }
-                },
-                function(error) {
-                    console.log("Internal Server");
-                }
-            );
-        }
-    }
+    //                 } else {
+    //                     alert("Data Not deleted !");
+    //                     $scope.GetVendor();
+    //                 }
+    //             },
+    //             function(error) {
+    //                 console.log("Internal Server");
+    //             }
+    //         );
+    //     }
+    // }
     $scope.onInit();
 })
