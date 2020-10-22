65eda6f278a5e0e939346dc83127e9f3eb20c38b
YuG <macos@macOSs-iMac.local>
2020-10-22 23:06:04 +0530

    increase body size

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var adminRouter = require('./routes/admin');
 var customerRouter = require('./routes/customer');
 var companyUserRouter = require('./routes/companyuser');
 var vendorUserRouter = require('./routes/vendor');
-
+var bodyParser = require('body-parser');
 var app = express();
 
 
@@ -17,7 +17,9 @@ app.use(cors());
 // app.use(express.limit(100000000));
 app.use(logger('dev'));
 // app.use(express.urlencoded());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(bodyParser.json({limit: '50mb'}));
+// app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
